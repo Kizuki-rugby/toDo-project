95d44000d24321ace92568eb72708dc520ad880b
koyasuk <koyasuk@lafayette.edu>
2023-04-21 11:49:29 -0400

    this is a test

--- toDo.js
+++ toDo.js
@@ -1,3 +1,21 @@
-console.log('hey this is cool');
-const interestRate = .3;
-console.log(interestRate);
+let person = {
+    name:'Kizuki', 
+    age: 30
+};
+//Dot Notation
+person.age = 21;
+person.name = 'Keigo';
+
+// Bracket Notation
+person['name'] = 'Kizuki';
+console.log(person);
+
+
+let array = ['red', 'blue', 'green'];
+  
+greet(person.name);
+
+function greet(name)
+{
+    console.log('hello ' + name);
+}
